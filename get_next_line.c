2f951ea12e34da5032dadd3e1c77281ed0205c76
Dovi KAPLAN <dkaplan@e5r11p5.wethinkcode.co.za>
2018-06-05 12:19:13 +0200

    Try

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dkaplan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/31 15:21:15 by dkaplan           #+#    #+#             */
-/*   Updated: 2018/06/04 16:58:48 by dkaplan          ###   ########.fr       */
+/*   Updated: 2018/06/05 12:16:15 by dkaplan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		returnplace(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0' && str[i] != '\n')
+	while (str[i] && str[i] != '\n')
 		i++;
 	return (i);
 }
@@ -33,10 +33,10 @@ int		get_next_line(int const fd, char **line)
 
 	if ((!(data = ft_strnew(BUFF_SIZE))) || (!(temp = ft_strnew(BUFF_SIZE))))
 		return (-1);
-	i = 1;
+	i = 0;
 	if (fd < 0 || !line || read(fd, data, 0) < 0)
 		return (-1);
-	read(fd, data, BUFF_SIZE);
+	/*read(fd, data, BUFF_SIZE);
 	data[BUFF_SIZE] = 0;
 	if (!(*line = ft_strdup(data)))
 		return (-1);
@@ -47,12 +47,36 @@ int		get_next_line(int const fd, char **line)
 		ft_strcpy(*line, data);
 		return (1);
 	}
-	else
-
 	while (!(ft_strchr(temp, '\n')))
 		read(fd, temp, BUFF_SIZE);
-	//printf("\n\n\nPRINTNUMBER%d\n\n\n", returnplace(data));
 	printf("line%s\n", line[0]);
 	//printf("temp%s\n", temp);
-	return (i);
+	return (i);*/
+	read(fd, data, BUFF_SIZE);
+	while (data[i] != '\n' && data[i] != '\0')
+	{
+		if (!data)
+			return (0);
+		if (data[i] == '\n' || data[i] == '\0')
+		{
+			ft_strcat(data, temp);
+			if (!(*line = ft_strdup(data)))
+			{
+				free(data);
+				return (-1);
+			}
+			free(data);
+			return (1);
+		}
+		if (i == ft_strlen(data))
+		{
+			if (!(*line = ft_strdup(data)))
+				return (-1);
+			read(fd, data, BUFF_SIZE);
+			ft_strcat(data, temp);
+		}
+		i++;
+	}
+	return(1);
+	free(data);
 }
--- main.c
+++ main.c
@@ -6,21 +6,25 @@
 /*   By: dkaplan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 10:19:09 by dkaplan           #+#    #+#             */
-/*   Updated: 2018/06/04 16:59:41 by dkaplan          ###   ########.fr       */
+/*   Updated: 2018/06/05 11:59:29 by dkaplan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <stdio.h>
 #include "get_next_line.h"
 
 int		main(int ac, char **av)
 {
 	(void)ac;
 	int fd;
-	char *line = "a";
+	char *line;
 
 	fd = open(av[1], O_RDONLY);
-	get_next_line(fd, &line);
-	free(line);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+	}
 	get_next_line(fd, &line);
 }	
