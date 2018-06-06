6127f5d278ed1e4c946b5ca830203865ae25a0fd
Dovi KAPLAN <dkaplan@e5r5p3.wethinkcode.co.za>
2018-06-06 14:21:05 +0200

    SUCCESS

---
+++ libft/ft_memalloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkaplan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/24 10:13:05 by dkaplan           #+#    #+#             */
+/*   Updated: 2018/05/24 14:02:39 by dkaplan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void	*res;
+
+	if (!(res = malloc(size * sizeof(*res))))
+		return (NULL);
+	ft_bzero(res, size);
+	return (res);
+}
